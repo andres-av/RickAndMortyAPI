aa3f7d7e1705775b59e4be13b48ab20708b2544b
andres-av <jandresav1793@gmail.com>
2022-08-01 19:56:04 +0000

    initial commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import starwars from "../../img/starwars-helmet.png"
+
+// Import Styles
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+    <>
+      <nav className="navbar bg-light">
+        <div className="container">
+          <div className="titleContainer">
+            <a className="navbar-brand" href="#">
+              <img src={starwars} alt="" width="55" height="55" />
+            </a>
+            <p className="navbarTitle">Star Wars</p>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn btn-danger">
+              Favorites
+            </button>
+            <button
+              type="button"
+              className="btn btn-danger dropdown-toggle dropdown-toggle-split"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <span className="visually-hidden">Toggle Dropdown</span>
+            </button>
+            <ul className="dropdown-menu">
+              <li>
+                <a className="dropdown-item" href="#">
+                  Ejemplo de Favorito
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 };
--- src/js/layout.js
+++ src/js/layout.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+import { CardsRow } from "./component/cardsRow";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -19,24 +17,21 @@ const Layout = () => {
 	return (
 		<div>
 			<BrowserRouter basename={basename}>
-				<ScrollToTop>
 					<Navbar />
 					<Switch>
 						<Route exact path="/">
 							<Home />
 						</Route>
-						<Route exact path="/demo">
-							<Demo />
+						<Route exact path="">
+							<CardsRow/>
 						</Route>
-						<Route exact path="/single/:theid">
-							<Single />
+						<Route exact path="">
 						</Route>
 						<Route>
 							<h1>Not found!</h1>
 						</Route>
 					</Switch>
 					<Footer />
-				</ScrollToTop>
 			</BrowserRouter>
 		</div>
 	);
