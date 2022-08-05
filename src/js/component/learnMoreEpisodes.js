bdd0fbfe59e60c2c7e4d250c0e78cad5f56d3f1e
andres-av <jandresav1793@gmail.com>
2022-08-05 18:01:39 +0000

    adding name of episodes on character description, issues at learnMoreEpisodes.js

--- src/js/component/learnMoreEpisodes.js
+++ src/js/component/learnMoreEpisodes.js
@@ -2,28 +2,28 @@
 import { element } from "prop-types";
 import React, { useState , useEffect } from "react";
 
-const LearnMoreEpisodes = (episode) => {
+const LearnMoreEpisodes = ({episode}) => {
+    
     const [ episodeName , setEpisodeName ] = useState([]);
 
-    const matrixEpisode = Object.keys(episode)
-    .map(function(key) {
-        return episode[key];
-    });
-// episodeArr is a matrix with the list of all episodes on position 0, so arrayOfEpisodes flattens the matrix into a 1 level array
-    const arrayOfEpisodes = matrixEpisode.flat();
-
     useEffect(() => {
-      arrayOfEpisodes.forEach((element) => {
-        fetch(element)
-          .then((response) => response.json())
-          .then((data) => {
-            setEpisodeName(data.name);
-          });
-      });
-    }, []);
+      if (episode) {
+        episode.forEach((element) => {
+          fetch(element)
+            .then((response) => response.json())
+            .then((data) => {
+              setEpisodeName([...episodeName , data.name]);
+              console.log(data.name);
+            });
+        });
+      }
+    }, [episodeName])
+
+      const episodeNames = episodeName.map((episode , index) => <li key={index}>{episode.name}</li>) 
 
     return(
-            <li>{episodeName}</li>
+        //  {episodeNames}
+        <li></li>
 )}
 
 export default LearnMoreEpisodes
